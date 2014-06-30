deb643d8701d16a6ce75f3c54673d4a5c2d09c86
Daniel Julius Lasiman <dj.lasiman@gmail.com>
2014-06-30 10:53:34 +0700

    Fix bug: gross_amount undefined

--- Veritrans/Vtdirect.php
+++ Veritrans/Vtdirect.php
@@ -4,17 +4,17 @@ class Veritrans_VtDirect {
 
   public static function charge($params)
   {
+    $payloads = array(
+        'payment_type' => 'credit_card'
+      );
+
     if (array_key_exists('item_details', $params)) {
       $gross_amount = 0;
       foreach ($params['item_details'] as $item) {
         $gross_amount += $item['quantity'] * $item['price'];
       }
+      $payloads['transaction_details']['gross_amount'] = $gross_amount;
     }
-
-    $payloads = array(
-        'payment_type' => 'credit_card',
-        'transaction_details' => array('gross_amount' => $gross_amount)
-      );
 
     $payloads = array_replace_recursive($payloads, $params);
 
--- Veritrans/Vtweb.php
+++ Veritrans/Vtweb.php
@@ -4,23 +4,21 @@ class Veritrans_Vtweb {
 
   public static function getRedirectionUrl($params)
   {
+    $payloads = array(
+        'payment_type' => 'vtweb',
+        'vtweb' => array(
+          'enabled_payments' => array('credit_card'),
+          'credit_card_3d_secure' => Veritrans::$is3ds
+        )
+      );
+
     if (array_key_exists('item_details', $params)) {
       $gross_amount = 0;
       foreach ($params['item_details'] as $item) {
         $gross_amount += $item['quantity'] * $item['price'];
       }
+      $payloads['transaction_details']['gross_amount'] = $gross_amount;
     }
-
-    $payloads = array(
-      'payment_type' => 'vtweb',
-      'vtweb' => array(
-        'enabled_payments' => array('credit_card'),
-        'credit_card_3d_secure' => Veritrans::$is3ds
-      ),
-      'transaction_details' => array(
-        'gross_amount' => $gross_amount
-      )
-    );
 
     $payloads = array_replace_recursive($payloads, $params);
 
