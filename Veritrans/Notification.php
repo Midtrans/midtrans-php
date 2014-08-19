0a6e610a01a570e90cbb63e42bdd0cf66568bddb
alvinlitani <alvin.litani@veritrans.co.id>
2014-08-19 14:53:11 +0700

    remove isVerified() function

--- Veritrans/Notification.php
+++ Veritrans/Notification.php
@@ -9,11 +9,6 @@ class Veritrans_Notification {
     $this->response = json_decode(file_get_contents('php://input'), true);
   }
 
-  public function isVerified()
-  {
-    return $this->response['notification_key'] == Veritrans_Config::$serverKey;
-  }
-
   public function __get($name)
   {
     if (array_key_exists($name, $this->response)) {
@@ -21,3 +16,5 @@ class Veritrans_Notification {
     }
   }
 }
+
+?>
--- examples/notification-handler.php
+++ examples/notification-handler.php
@@ -5,30 +5,31 @@ require_once(dirname(__FILE__) . '/../Veritrans.php');
 Veritrans_Config::$serverKey = '<your server key>';
 
 $notif = new Veritrans_Notification();
-if ($notif->isVerified()) {
-  $transaction = $notif->transaction_status;
-  $fraud = $notif->fraud_status;
 
-  error_log("Order ID $notif->order_id: " .
-      "transaction status = $transaction, fraud staus = $fraud");
+$transaction = $notif->transaction_status;
+$fraud = $notif->fraud_status;
 
-  if ($transaction == 'capture') {
-    if ($fraud == 'challenge') {
-      // TODO Set payment status in merchant's database to 'challenge'
-    }
-    else if ($fraud == 'accept') {
-      // TODO Set payment status in merchant's database to 'success'
-    }
-  }
-  else if ($transaction == 'cancel') {
-    if ($fraud == 'challenge') {
-      // TODO Set payment status in merchant's database to 'failure'
-    }
-    else if ($fraud == 'accept') {
-      // TODO Set payment status in merchant's database to 'failure'
-    }
-  }
-  else if ($transaction == 'deny') {
-      // TODO Set payment status in merchant's database to 'failure'
-  }
+error_log("Order ID $notif->order_id: " .
+  "transaction status = $transaction, fraud staus = $fraud");
+
+if ($transaction == 'capture') {
+	if ($fraud == 'challenge') {
+	  // TODO Set payment status in merchant's database to 'challenge'
+	}
+	else if ($fraud == 'accept') {
+	  // TODO Set payment status in merchant's database to 'success'
+	}
 }
+else if ($transaction == 'cancel') {
+	if ($fraud == 'challenge') {
+	  // TODO Set payment status in merchant's database to 'failure'
+	}
+	else if ($fraud == 'accept') {
+	  // TODO Set payment status in merchant's database to 'failure'
+	}
+}
+else if ($transaction == 'deny') {
+  // TODO Set payment status in merchant's database to 'failure'
+}
+
+?>
