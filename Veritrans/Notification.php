dc20b7babd45c78543d93991f2a349f1e8cea1ee
Pavel <pavel.evst@gmail.com>
2015-01-19 13:34:52 +0700

    Add tests for Veritrans_Notification

--- Veritrans/Notification.php
+++ Veritrans/Notification.php
@@ -4,9 +4,9 @@ class Veritrans_Notification {
 
   private $response;
 
-  public function __construct()
+  public function __construct($input_source = "php://input")
   {
-    $this->response = json_decode(file_get_contents('php://input'), true);
+    $this->response = json_decode(file_get_contents($input_source), true);
   }
 
   public function __get($name)
