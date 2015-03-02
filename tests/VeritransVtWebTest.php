fd59619a366419b873ec72d800b161d3dd665890
Pavel <pavel.evst@gmail.com>
2015-03-02 18:33:47 +0700

    More tests for VT-Web

--- tests/VeritransVtWebTest.php
+++ tests/VeritransVtWebTest.php
@@ -9,10 +9,7 @@ class VeritransVtWebTest extends PHPUnit_Framework_TestCase
     public function testGetRedirectionUrl() {
       Veritrans_Config::$serverKey = 'My Very Secret Key';
       VT_Tests::$stubHttp = true;
-      VT_Tests::$stubHttpResponse = '{
-        "status_code": 200,
-        "redirect_url": "http://host.com/pay"
-      }';
+      VT_Tests::$stubHttpResponse = '{ "status_code": 200, "redirect_url": "http://host.com/pay" }';
 
       $params = array(
         'transaction_details' => array(
@@ -41,6 +38,47 @@ class VeritransVtWebTest extends PHPUnit_Framework_TestCase
       $this->assertEquals($fields["POSTFIELDS"],
         '{"payment_type":"vtweb","vtweb":{"credit_card_3d_secure":false},' . 
         '"transaction_details":{"order_id":"Order-111","gross_amount":10000}}'
+      );
+
+      VT_Tests::reset();
+    }
+
+    public function testGrossAmount() {
+      $params = array(
+        'transaction_details' => array(
+          'order_id' => rand()
+        ),
+        'item_details' => array( array( 'price' => 10000, 'quantity' => 5 ) )
+      );
+
+      VT_Tests::$stubHttp = true;
+      VT_Tests::$stubHttpResponse = '{ "status_code": 200, "redirect_url": "http://host.com/pay" }';
+
+      $paymentUrl = Veritrans_Vtweb::getRedirectionUrl($params);
+
+      $this->assertEquals(
+        VT_Tests::$lastHttpRequest['data_hash']['transaction_details']['gross_amount'],
+        50000
+      );
+
+      VT_Tests::reset();
+    }
+
+    public function testOverrideParams() {
+      $params = array(
+        'vtweb' => array(
+          'extra' => 'param'
+        )
+      );
+
+      VT_Tests::$stubHttp = true;
+      VT_Tests::$stubHttpResponse = '{ "status_code": 200, "redirect_url": "http://host.com/pay" }';
+
+      $paymentUrl = Veritrans_Vtweb::getRedirectionUrl($params);
+
+      $this->assertEquals(
+        VT_Tests::$lastHttpRequest['data_hash']["vtweb"],
+        array("credit_card_3d_secure" => false, "extra" => "param")
       );
 
       VT_Tests::reset();
