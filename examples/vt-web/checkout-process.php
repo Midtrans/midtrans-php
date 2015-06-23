3da79c56a8851f832fe5804aaa82c0d86beb649e
Yocki <yocki-s@users.noreply.github.com>
2015-06-23 18:23:17 +0700

    fix confusing input server key
    Some merrchant input server key in wrong place, so they always get error result

--- examples/vt-web/checkout-process.php
+++ examples/vt-web/checkout-process.php
@@ -1,9 +1,6 @@
 <?php
-
-require_once(dirname(__FILE__) . '/../../Veritrans.php');
-
-Veritrans_Config::$serverKey = '<your server key>';
-
+require_once('Veritrans.php');
+Veritrans_Config::$serverKey = "<your server key>";
 // Uncomment for production environment
 // Veritrans_Config::$isProduction = true;
 
@@ -13,27 +10,28 @@ Veritrans_Config::$serverKey = '<your server key>';
 // Uncomment to enable 3D-Secure
 // Veritrans_Config::$is3ds = true;
 
-if (Veritrans_Config::$serverKey == '<your server key>') {
-  echo "<code>";
-  echo "<h4>Please set real server key from sandbox</h4>";
-  echo "In file: " . __FILE__;
-  echo "<br>";
-  echo "<br>";
-  echo htmlspecialchars('Veritrans_Config::$serverKey = \'<your server key>\';');
-  die();
-}
-
 $params = array(
     'transaction_details' => array(
       'order_id' => rand(),
       'gross_amount' => 10000,
     )
   );
-
 try {
   // Redirect to Veritrans VTWeb page
   header('Location: ' . Veritrans_VtWeb::getRedirectionUrl($params));
 }
 catch (Exception $e) {
   echo $e->getMessage();
+  if(strpos ($e->getMessage(), "Access denied due to unauthorized")){
+      echo "<code>";
+      echo "<h4>Please set real server key from sandbox</h4>";
+      echo "In file: " . __FILE__;
+      echo "<br>";
+      echo "<br>";
+      echo htmlspecialchars('Veritrans_Config::$serverKey = \'<your server key>\';');
+      die();
 }
+
+}
+
+?>
