43d5252b2ced089594641cee29064aafc29edf63
Yocki <yocki-s@users.noreply.github.com>
2015-06-26 11:06:52 +0700

    fix path

--- examples/vt-web/checkout-process.php
+++ examples/vt-web/checkout-process.php
@@ -1,5 +1,5 @@
 <?php
-require_once('Veritrans.php');
+require_once(dirname(__FILE__) . '/../../Veritrans.php');
 Veritrans_Config::$serverKey = "<your server key>";
 // Uncomment for production environment
 // Veritrans_Config::$isProduction = true;
