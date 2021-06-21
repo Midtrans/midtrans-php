9711d725ffe713af384ffb0937428dfd4738b6eb
Zaki Ibrahim <xaxxis.id@gmail.com>
2021-06-21 11:09:16 +0700

    REFACTOR UNIT TEST-Rename Vt prefix to MT

--- test_bootstrap.php
+++ test_bootstrap.php
@@ -6,5 +6,5 @@
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 require_once dirname(__FILE__) . '/Midtrans.php';
-require_once dirname(__FILE__) . '/tests/VtTests.php';
-require_once dirname(__FILE__) . '/tests/utility/VtFixture.php';
+require_once dirname(__FILE__) . '/tests/Mt_Tests.php';
+require_once dirname(__FILE__) . '/tests/utility/MtFixture.php';
