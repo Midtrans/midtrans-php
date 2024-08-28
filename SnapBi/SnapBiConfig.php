7882e647efdab1031a282d85b5d206da135e634d
muhammad fauzi masykur <muhammad.masykur@gojek.com>
2024-08-28 15:22:12 +0700

    create SnapBiConfig

---
+++ SnapBi/SnapBiConfig.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SnapBi;
+
+/**
+ * Midtrans Configuration
+ */
+class SnapBiConfig
+{
+    public static $isProduction = false;
+    public static $snapBiClientId;
+    public static $snapBiPrivateKey;
+    public static $snapBiClientSecret;
+    public static $snapBiPartnerId;
+    public static $snapBiChannelId;
+
+    const SNAP_BI_SANDBOX_BASE_URL = 'https://merchants.stg.midtrans.com';
+    const SNAP_BI_PRODUCTION_BASE_URL = 'https://merchants.midtrans.com';
+
+    /**
+     * Get baseUrl
+     * 
+     * @return string Midtrans API URL, depends on $isProduction
+     */
+    public static function getSnapBiTransactionBaseUrl()
+    {
+        return SnapBiConfig::$isProduction ?
+            SnapBiConfig::SNAP_BI_PRODUCTION_BASE_URL : SnapBiConfig::SNAP_BI_SANDBOX_BASE_URL;
+    }
+}
