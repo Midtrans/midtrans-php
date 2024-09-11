190f99c37b6058c36ca54e09f3794923f5d98353
muhammad fauzi masykur <muhammad.masykur@gojek.com>
2024-09-12 01:44:25 +0700

    enable to show  body when enable logging

--- SnapBi/SnapBiApiRequestor.php
+++ SnapBi/SnapBiApiRequestor.php
@@ -16,22 +16,30 @@ class SnapBiApiRequestor
             $curlHeaders[] = "$key: $value";
         }
 
-        $payload_json = json_encode($body);
+        $payload_json = json_encode($body, JSON_PRETTY_PRINT);
+
+        if (SnapBiConfig::$enableLogging){
+            echo "Request Body: " .PHP_EOL . $payload_json . PHP_EOL;
+        }
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $curlHeaders);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload_json);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, SnapBiConfig::$enableLogging);
 
         $response = curl_exec($ch);
 
         if (curl_errno($ch)) {
             throw new Exception(curl_error($ch));
         }
+        $jsonResponse = json_decode($response);
         curl_close($ch);
-
-        return json_decode($response);
+        if (SnapBiConfig::$enableLogging){
+            echo "Response body: " .PHP_EOL . json_encode($jsonResponse, JSON_PRETTY_PRINT) . PHP_EOL;
+        }
+        
+        return $jsonResponse;
     }
 }
